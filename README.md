0a97bf5703bdfe3ec0ee457c38a5394a72a3bda8
Malone Hedges <me@malone.io>
2017-10-05 01:51:51 +0200

    :tada: Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# malone.io
+
+v4 of my personal website.
+
+This time around, making it prettier with a big image and simplifying the amount of text further.
---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
+const del = require('del')
+const path = require('path')
+const ghpages = require('gh-pages')
+const runSequence = require('run-sequence')
+const revision = require('git-rev')
+const minify = require('gulp-minifier')
+
+// Default Task - set to fail to avoid accidental publishing
+gulp.task('default', () => {
+  console.log('Use gulp deploy to deploy to master.')
+})
+
+gulp.task('build-css', () => {
+  return gulp.src(['src/**/*.css'])
+    .pipe(autoprefixer(['> 5%']))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('build', ['build-css'], () => {
+  return gulp.src(['src/**/!(*.css)'])
+    .pipe(minify({
+      minify: true,
+      collapseWhitespace: true,
+      minifyJS: true,
+      minifyCSS: true,
+    }))
+    .pipe(gulp.dest('build'))
+})
+
+gulp.task('clean', () => {
+  return del(['build'])
+})
+
+gulp.task('github', (cb) => {
+  revision.short((rev) => {
+    ghpages.publish(path.join(__dirname, 'build'), {
+      branch: 'master',
+      message: 'Update to ' + rev + '.',
+    }, cb)
+  })
+})
+
+gulp.task('dev', () => {
+  browserSync.init({
+    server: './src',
+  })
+
+  gulp.watch("src/**/*").on('change', browserSync.reload)
+})
+
+gulp.task('deploy', (cb) => {
+  runSequence('clean', 'build', 'github', cb)
+})
